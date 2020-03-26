fe0d3731613cb1b135a61971b2160837b383be16
Melykuti <bence.melykuti@gmail.com>
2020-03-26 09:45:06 +0100

    Updates JHU download script with inclusion of recovered patient numbers.

--- code/download_JHU_CSSE.py
+++ code/download_JHU_CSSE.py
@@ -1,6 +1,6 @@
 '''
 exec(open('download_JHU_CSSE.py','r').read())
-12/3, 24/3/2020
+12/3, 24/3, 26/3/2020
 '''
 
 import os, urllib.request, time
@@ -8,9 +8,10 @@ import os, urllib.request, time
 #files = ['https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv',
 #'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv',
 #'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv'] # until 23 March 2020
-# Since 24 March 2020
+# Since 24 March 2020. On 26 March time_series_covid19_recovered_global.csv added.
 files = ['https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv',
-'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv']
+'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv',
+'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv']
 
 datetime_str = time.strftime('%Y%m%d_%H-%M-%S', time.localtime(time.time()))
 for source in files:
